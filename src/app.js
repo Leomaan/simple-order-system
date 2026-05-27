cbd873376c07d5af2fafea7f044bb8fb73abea78
Leomaan <146137607+Leomaan@users.noreply.github.com>
2026-05-27 16:49:50 -0300

    routes updated

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import e from "express";
-import productRoutes from "./routes/produtctRoutes.js"
-import orderRoutes from "./routes/orderRoutes.js"
-import orderItemRoutes from "./routes/orderItemRoutes.js"
+import productRoutes from "./routes/produtctRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import orderItemRoutes from "./routes/orderItemRoutes.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = e();
 
 app.use(e.json());
 
-app.use('/product', productRoutes)
-app.use('/order', orderRoutes)
-app.use('/order-item', orderItemRoutes)
+app.use('/product', productRoutes);
+app.use('/order', orderRoutes);
+app.use('/order-item', orderItemRoutes);
+
+app.use(errorHandler);
 
 export default app;
-
--- src/routes/orderItemRoutes.js
+++ src/routes/orderItemRoutes.js
@@ -1,9 +1,10 @@
-import { create, remove } from "../controllers/orderItemController.js";
+import { create, remove, changeQuantity } from "../controllers/orderItemController.js";
 import { Router } from "express";
 
 const routes = Router();
 
 routes.post('/', create);
 routes.delete('/:id', remove);
+routes.patch('/:id', changeQuantity);
 
 export default routes;
